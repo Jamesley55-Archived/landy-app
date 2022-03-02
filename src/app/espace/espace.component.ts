ef5dc3eaf1e920e86ac1ba1ee7189d7fdd2a9093
Jamesley555 <jamesley.joseph@polymtl.ca>
2022-03-02 04:21:21 -0500

    fix: add code for espace

--- src/app/espace/espace.component.ts
+++ src/app/espace/espace.component.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+declare function espace(): any;
 @Component({
   selector: 'app-espace',
   templateUrl: './espace.component.html',
-  styleUrls: ['./espace.component.css']
+  styleUrls: ['./espace.component.css'],
 })
 export class EspaceComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    espace();
   }
-
 }
 
-
 /**
- * 
- * 
-*/
+ *
+ *
+ */
