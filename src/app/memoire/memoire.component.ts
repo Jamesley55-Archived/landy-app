3eceffb0caefd3367e2f628d9c97b9f9cb51bf6d
Jamesley555 <jamesley.joseph@polymtl.ca>
2022-03-02 04:52:35 -0500

    fix: add code for espace

--- src/app/memoire/memoire.component.ts
+++ src/app/memoire/memoire.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare function memoire(): any;
+declare function reset(): any;
 
 @Component({
   selector: 'app-memoire',
   templateUrl: './memoire.component.html',
-  styleUrls: ['./memoire.component.css']
+  styleUrls: ['./memoire.component.css'],
 })
 export class MemoireComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    memoire();
   }
-
+  ngAfterViewInit() {
+    reset();
+  }
 }
-
